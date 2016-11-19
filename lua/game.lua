98cadd0782f0ec49667c79ab341a38da388a0f00
heml <maolong.ppsg.com>
2016-11-19 17:37:56 +0800

    split nginx conf

--- lua/game.lua
+++ lua/game.lua
@@ -5,13 +5,13 @@ local conf = require 'conf_server'
 local redis = require "resty.redis"
 local game = {}
 
-function game.poll(data)
-	local args = cjson.decode(data)
-	if not (args and args.server_id) then
+function game.poll(args)
+	local data = cjson.decode(args)
+	if not (data and data.server_id) then
 		return 50, 'invalid server_id'
 	end
 
-	if not args.server_id then
+	if not data.server_id then
 		return 51, 'server id lost'
 	end
 
@@ -24,26 +24,31 @@ function game.poll(data)
 		return 52, 'redis lost'
 	end
 
-  local chan = "channel:game:"..args.server_id
+  local chan = "channel:game:"..data.server_id
 	local sub_rep, sub_err = red:subscribe(chan)
   if not sub_rep then
     return 53, 'failed to subscribe: ' .. sub_err
   end
+	redis:set_keepalive(1000*300, 100)  
 
-	redis:set_keepalive(1000*60, 100)  
+	local ok, err = ngx.on_abort(function()
+		 ngx.log(ngx.INFO, 'client abort ', data.server_id)
+		 ngx.exit(500)
+		end)
   while true do
 	  local read_data, read_err = red:read_reply()
   	if not read_data then
   		if read_err ~= 'timeout' then
-  			-- TODO log
   			break
   		end
   	else
 			redis:unsubscribe(chan)
-		  --ngx.log(ngx.INFO, 'poll ', args.server_id)
+		  --ngx.log(ngx.DEBUG, 'poll ', data.server_id)
 		  return 0, read_data
   	end
 	end
+
+
 end
 
 return game
--- lua/mail.lua
+++ lua/mail.lua
@@ -1,59 +1,71 @@
 local cjson = require 'cjson'
 local conf = require 'conf_server'
-local redis = require "resty.redis":new()
+local redis = require "resty.redis"
 
 local mail = {}
 
+local function _get_mail_redis()
+	local red = redis:new()
+  redis:set_timeout(1000*5)
+	local redis_conf = conf.redis.poll
+	local ok, err = red:connect(redis_conf.host, redis_conf.port)
+	if not ok then
+
+		return
+	end
+end
+
 function mail.init(data)
-	local player_id, server_id = ARGV[1], ARGV[2]
+	local player_id, server_id = data.player_id, data.server_id
 	if player_id <= 0 or server_id <= 0 then
 		return {100, 'invalid player_id or server_id'}
 	end
 
 	local player_key = 'player:' .. player_id
-	redis.call('hset', player_key, 'server_id', server_id)
-	redis.call('expire', player_key, 86400*35)
-	redis.call('srem', 'serverplayer:'..server_id, player_id)
-	return {0, 'OK'}
+	red.call('hset', player_key, 'server_id', server_id)
+	red.call('expire', player_key, 86400*35)
+	red.call('srem', 'serverplayer:'..server_id, player_id)
+	return 0, 'OK'
 end
 
+-- ENSURE the mail is self mail
 function mail.get(data)
-	local player_id = ARGV[1]
-	local now = ARGV[2]
+	local player_id = data.player_id
+	local now = ngx.time()
 	if not player_id or player_id <= 0 then return {300, 'invalid player_id'} end
 
 	local player_key = 'player:'..player_id
-	local result = redis.call('hmget', player_key, 'server_id', 'version')
+	local result = red.call('hmget', player_key, 'server_id', 'version')
 	local server_id, version = result[1], result[2] or now
 
 	if not server_id then
-		return {301, 'invalid player server'}
+		return 301, 'invalid player server'
 	end
 
-	local begin = now = 86400 * 20
+	local begin = now - 86400 * 20
 	if version < begin then
 		version = begin
 	end
 
-	local server_mails = redis.call('zrangebyscore', 'servermail:' .. server_id, now, version+1, 'limit', 0, 30)
-	redis.call('zadd', player_key, 'version', now)
+	local server_mails = red.call('zrangebyscore', 'servermail:' .. server_id, now, version+1, 'limit', 0, 30)
+	red.call('zadd', player_key, 'version', now)
 	for _, v in ipairs(server_mails) do
-		local send_ts = redis.call('hget', v, 'ts')
+		local send_ts = red.call('hget', v, 'ts')
 		if send_ts then
-			redis.call('zadd', player_key, 'NX', v, send_ts)
+			red.call('zadd', player_key, 'NX', v, send_ts)
 		end
 	end
 end
 
 function mail.delete(data)
-	local player_id, mail_id = ARGV[1], ARGV[2]
+	local player_id, mail_id = data.player_id, data.mail_id
 	if player_id == 0 or not mail_id then return {300, 'error: 1'} end
 	local player_key = 'player:'..player_id
-	if redis.call('hdel', player_key, mail_id) == 0 then
-		return {301, 'error: player have no mail '.. player_id .. ' ' .. mail_id}
+	if red.call('hdel', player_key, mail_id) == 0 then
+		return 301, 'error: player have no mail '.. player_id .. ' ' .. mail_id
 	end
-	if redis.call('del', mail_id) then
-		return {302, 'error: mail not exist '.. mail_id}
+	if red.call('del', mail_id) then
+		return 302, 'error: mail not exist '.. mail_id
 	end
 	return {0}
 end
@@ -64,17 +76,18 @@ function mail.send(data)
 		return 200, 'invalid mail data'
 	end
 
-	local server_id, player_id, form_id, title, content, atts, count = 
-	  mail_data.server_id, mail_data.player_id, mail_data.form_id, mail_data.title, mail_data.content, mail_data.atts, mail_data.count
+  local now = ngx.time()
+	local mail_id, player_id, form_id, title, content, atts = 
+	  mail_data.mail_id, mail_data.player_id, mail_data.form_id, mail_data.title, mail_data.content, mail_data.atts
 
-	if server_id <= 0 and player_id <= 0 then
-	  return {201, 'invalid params'} end
+	if mail_id <= 0 and player_id <= 0 then
+	  return 201, 'invalid params'
 	end
 
-	local mail_params = {'ts', ts}
+	local mail_params = {'ts', now}
 	if form_id > 0 then
-		mail_params[#mail_params+1] = 'mail_id'
-		mail_params[#mail_params+1] = mail_id
+		mail_params[#mail_params+1] = 'form_id'
+		mail_params[#mail_params+1] = form_id
 	end
 	if title and #title > 0 then
 		mail_params[#mail_params+1] = 'title'
@@ -92,26 +105,26 @@ function mail.send(data)
 	-- 	mail_params[#mail_params+1] = 'atts_mask'
 	-- 	mail_params[#mail_params+1] = atts_mask
 	-- end
-	if count > 0 then
-		mail_params[#mail_params+1] = 'count'
-	    mail_params[#mail_params+1] = count
-	end
+	-- if count > 0 then
+	-- 	mail_params[#mail_params+1] = 'count'
+	--     mail_params[#mail_params+1] = count
+	-- end
 	if #mail_params <= 0 then return {201, 'invalid params'} end
 
 	local mail_key = 'mail:'..player_id..':'..new_mail_id
-	redis.call('hmset', mail_key, unpack(mail_params))
-	redis.call('expire', mail_key, 86400*35)
+	red.call('hmset', mail_key, unpack(mail_params))
+	red.call('expire', mail_key, 86400*35)
 	if player_id <= 0 then
 		-- Broadcast mails, for all server player
-		redis.call('zadd', 'servermail:' .. server_id, ts, mail_key) -- Used to notify player
+		red.call('zadd', 'servermail:' .. server_id, ts, mail_key) -- Used to notify player
 	else
-		local new_mail_id = redis.call('incr', 'mail_id')
+		local new_mail_id = red.call('incr', 'mail_id')
 		-- Normal mails
-		redis.call('sadd', 'player:' .. player_id, mail_key, ts)
-		redis.call('sadd', 'serverplayer:'..server_id, player_id) -- Used to notify player
+		red.call('sadd', 'player:' .. player_id, mail_key, ts)
+		red.call('sadd', 'serverplayer:'..server_id, player_id) -- Used to notify player
 	end
 
-	return {0}
+	return 0
 end
 
 
